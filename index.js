9f8e27dbf2457c3eb9aaf2675718dfce0d1c324e
byoung730 <brian.young@monsanto.com>
2018-09-20 16:34:16 -0500

    beginning google oauth

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-app.get("/", (req, res) => {
-  res.send({ what: "it do" });
-});
+const app = express();
+
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
