49ea5343c937010b9910ee6a61b93294ecaaedd5
byoung730 <brian.young@monsanto.com>
2018-09-21 18:24:16 -0500

    mongo connect

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+
+const options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: 100, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+
+mongoose
+  .connect(
+    keys.mongoURI,
+    { useNewUrlParser: true },
+    options
+  )
+  .then(
+    () => {
+      console.log("connected to mongoDB");
+    },
+    err => {
+      console.log("err", err);
+    }
+  );
 
 const app = express();
 
