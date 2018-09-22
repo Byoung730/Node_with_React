91e40ae0f0abffe7e808df2ab22834b53b95c43e
byoung730 <brian.young@monsanto.com>
2018-09-22 11:35:39 -0500

    db user flow

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,15 @@ passport.use(
       callbackURL: "http://localhost:5000/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token: ", accessToken);
-      console.log("refresh token: ", refreshToken);
-      console.log("profile: ", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        existingUser
+          ? done(null, existingUser)
+          : new User({
+              googleId: profile.id,
+              name: profile.displayName
+            }).save(),
+          then(user => done(null, user));
+      });
     }
   )
 );
