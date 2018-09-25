edb27825a96f2779a6e0046153199cef659cf2e8
byoung730 <brian.young@monsanto.com>
2018-09-25 14:03:00 -0500

    cookies

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id); //not googleId in case of multiple oauths;
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
